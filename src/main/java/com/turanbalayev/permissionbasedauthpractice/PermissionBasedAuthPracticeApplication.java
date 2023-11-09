87e1f38f41d9b8bf7b9abef672ef578b8b6ccc83
Turan Balayev <turanbalayev96@gmail.com>
2023-11-09 20:23:29 +0400

    Add needed dependencies

---
+++ src/main/java/com/turanbalayev/permissionbasedauthpractice/PermissionBasedAuthPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.turanbalayev.permissionbasedauthpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PermissionBasedAuthPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PermissionBasedAuthPracticeApplication.class, args);
+	}
+
+}
