1937c27880fd60f9d52db7824f11ffdaa8d69d41
Turan Balayev <turanbalayev96@gmail.com>
2023-12-04 22:00:47 +0400

    Remove useless comment

--- src/main/java/com/turanbalayev/permissionbasedauthpractice/PermissionBasedAuthPracticeApplication.java
+++ src/main/java/com/turanbalayev/permissionbasedauthpractice/PermissionBasedAuthPracticeApplication.java
@@ -78,62 +78,12 @@ public class PermissionBasedAuthPracticeApplication {
         };
     }
 
-    private static void extractedMethod(PermissionService permissionService, RoleService roleService, UserService userService) {
-        // Create book:read, book:write, product:read, product:write permissions
-        Permission bookRead = permissionService.savePermission("book:read");
-        Permission bookWrite = permissionService.savePermission("book:write");
-        Permission productRead = permissionService.savePermission("product:read");
-        Permission productWrite = permissionService.savePermission("product:write");
+    /*
 
-        log.info("These permissions added: {} {} {} {}",
-                bookRead.getName(),
-                bookWrite.getName(),
-                productRead.getName(),
-                productWrite.getName());
-/*
-		// Create role_user, role_admin
-		Role role_user = roleService.saveRole("ROLE_USER");
-		Role role_admin = roleService.saveRole("ROLE_ADMIN");
-
-		// Add permissions to a user role
-		roleService.addPermissionToARole(bookRead.getName(),role_user.getName());
-		roleService.addPermissionToARole(bookWrite.getName(),role_user.getName());
-
-		// Add permissions to an admin role
-		roleService.addPermissionToARole(bookRead.getName(),role_admin.getName());
-		roleService.addPermissionToARole(bookWrite.getName(),role_admin.getName());
-		roleService.addPermissionToARole(productRead.getName(),role_admin.getName());
-		roleService.addPermissionToARole(productWrite.getName(),role_admin.getName());*/
-
-
-        // Create users
-		/*User turan = userService.saveUser(new User("Turan","Balayev","turan_balayev@gmail.com",
-				passwordEncoder.encode("12345")));
-		User alex = userService.saveUser(new User("Alex","Stones","alex_stones@gmail.com",
-				passwordEncoder.encode("12345")));*/
-
-
-/*		// Add roles to users
-		userService.addRoleToUser(role_user.getName(),turan.getEmail());
-		userService.addRoleToUser(role_user.getName(),alex.getEmail());
-		userService.addRoleToUser(role_admin.getName(),alex.getEmail());
-
-		List<Role> rolesOfAlex = userService.getRolesOfUserByUserId(alex.getId());
-
-
-		rolesOfAlex.forEach(role -> {
-			log.info("{} has a role: {}",alex.getFirstName(),role.getName());
-
-			roleService.getPermissionOfRoleByRoleName(role.getName()).forEach(
-					(p) -> {log.info("Role {} has permission {}",role.getName(),p.getName());}
-			);
-
-		});*/
-    }
-/*
-	@Bean
+    @Bean
 	PasswordEncoder passwordEncoder(){
 		return new BCryptPasswordEncoder();
+
 	}*/
 
 }
